8a874101d61e343dc509cc7495b84bb2c43f2b0d
Raphael Akpan <raphael.akpan@andela.com>
2019-01-18 13:58:41 +0100

    Hide pagination until there is data

--- components/Pagination/index.jsx
+++ components/Pagination/index.jsx
@@ -3,40 +3,44 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import StyledPagination from '../styles/Pagination';
 
-const Pagination = ({ page, perPage, count, pathname, name}) => {
+const Pagination = ({ page, perPage, count, pathname, name }) => {
   const pages = Math.ceil(count / perPage);
-  if (count < 1 || page >= pages) return null;
+  if (count < 1 || page > pages || (page === 1 && page === pages)) return null;
   return (
     <StyledPagination>
       <Head>
-        <title>Flex Fits - Page {page} of {pages} | {name} </title>
+        <title>
+          Flex Fits - Page {page} of {pages} | {name}{' '}
+        </title>
       </Head>
       <Link
         prefetch
         href={{
           pathname,
-          query: {page: page - 1}
+          query: { page: page - 1 },
         }}
       >
         <a className="prev" aria-disabled={page <= 1}>
-          <i className="far fa-arrow-alt-circle-left"></i> Prev
+          <i className="far fa-arrow-alt-circle-left" /> Prev
         </a>
       </Link>
-      <p> Page {page} of {pages} </p>
+      <p>
+        Page {page} of {pages}
+      </p>
       <Link
         prefetch
         href={{
           pathname,
-          query: { page: page + 1 }
+          query: { page: page + 1 },
         }}
       >
         <a className="prev" aria-disabled={page >= pages}>
-          Next <i className="far fa-arrow-alt-circle-right"></i>
+          Next <i className="far fa-arrow-alt-circle-right" />
         </a>
       </Link>
     </StyledPagination>
   );
-}
+};
 
 Pagination.propTypes = {
   page: PropTypes.number.isRequired,
